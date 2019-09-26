101f10dcc898bc68712b303ad4c4cc0b324a7eab
Sandeep Chandran <sandeep@paypossible.com>
2019-09-26 09:08:47 -0500

    react test

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ background: "grey", textAlign: "center", height: "100vh" }}>
+      <h1 style={{ color: "white" }}>App Page</h1>
+    </div>
+  );
+};
+
+export default App;
