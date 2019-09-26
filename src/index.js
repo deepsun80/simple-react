d552a02d7a9ccef8cc82e3b18bcad3d72763d28a
Sandeep Chandran <sandeep@paypossible.com>
2019-09-26 10:18:15 -0500

    route added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <Route exact path="/" render={() => <Redirect to="/app" />} />
+    <Route path="/app" component={App} />
+  </Router>,
+  document.getElementById("root")
+);
 
 module.hot.accept();
